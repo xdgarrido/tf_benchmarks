0c680ce311d3391e65857eb7bcd80c657cbfcbb3
diego <diego.garrido@amd.com>
2020-09-22 13:47:35 -0700

    add attention search

--- attention.sh
+++ attention.sh
@@ -76,8 +76,6 @@ for BATCH in  8 16 32
      do 
       for HEADS in 8 16 32 64
         do
-          for SIZE in 16 32 64 
-            do
                 echo $BATCH:$LENGTH:$HEADS:$SIZE
                 # run pretraining
                 echo "[ATTENTIONHEAD]" >> $SCRIPTPATH/eval_results.txt
@@ -88,15 +86,13 @@ for BATCH in  8 16 32
                 echo "BATCH_SIZE=$BATCH" >> $SCRIPTPATH/eval_results.txt
                 echo "SEQ_LENGTH=$LENGTH" >> $SCRIPTPATH/eval_results.txt
                 echo "HEADS=$HEADS" >> $SCRIPTPATH/eval_results.txt
-                echo "SIZE_ATTENTION_HEAD=$SIZE" >> $SCRIPTPATH/eval_results.txt
                 starttime=$(date +%s)
                 docker exec $CTNRNAME python3 $CODE_DIR_INSIDE/tf_benchmarks/Attention/attention.py --iter=$COUNT \
-                --seq_length=$LENGTH --batch=$BATCH --precision=$PRECISION --num_attention_heads=$HEADS --attention_head_size=$SIZE \
+                --precision=$PRECISION --seq_length=$LENGTH --batch=$BATCH --attention_heads=$HEADS --mode=$MODE \
                 --mode=$MODE 2>&1 | tee $SCRIPTPATH/Attention/log.txt
                 endtime=$(date +%s)
                 echo "ELAPSED_TIME(in secs)=$((${endtime} - ${starttime}))" >> $SCRIPTPATH/eval_results.txt
                 let "count++"
-            done
         done 
     done
 done 
